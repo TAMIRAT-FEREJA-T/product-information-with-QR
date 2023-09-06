0456fc720e0be9c82f6d3a9b6f9ecf1125bc27fa
TAMIRAT-FEREJA-T <100931093+TAMIRAT-FEREJA-T@users.noreply.github.com>
2023-09-06 09:43:40 -0600

    Update script.js
    sanitized input field

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -27,16 +26,23 @@ var serial_number = document.getElementById("serialnumber");
 var prod_model = document.getElementById("productmodel");
 var prod_name = document.getElementById("productname");
 var company_name = document.getElementById("companyname");
-var prod_date = document.getElementById("productiondate");    
+var prod_date = document.getElementById("productiondate");
 var exp_date = document.getElementById("expiredate");
-
 
 var insbtn = document.getElementById("submit");
 
 // insert data
+
 function insertdata() {
-  var production_date= prod_date.value;
-  var expire_date= exp_date.value;
+  // Check if any of the input fields are empty.
+  if (serial_number.value === "" || prod_model.value === "" || prod_name.value === "" || company_name.value === "" || prod_date.value === "" || exp_date.value === "") {
+    alert("Please fill all data.");
+    return;
+  }
+
+  // Otherwise, insert the data into Firebase.
+  var production_date = prod_date.value;
+  var expire_date = exp_date.value;
 
   set(ref(db, "products/" + serial_number.value), {
     serialnumber: serial_number.value,
@@ -46,14 +52,18 @@ function insertdata() {
     productiondate: production_date,
     expiredate: expire_date
   })
-    .then(() => {
 
+    .then(() => {
       alert("Data inserted");
-      open('/confiry.html');
+      open('/confiry/index.html');
     })
     .catch((error) => {
       alert("unsuccessful ,error" + error);
     });
 }
+
+// -------------------show product
+
+// -----------
 
 insbtn.addEventListener('click', insertdata);
